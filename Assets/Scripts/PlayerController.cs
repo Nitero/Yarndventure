3f81baf3266e24bf777e182c51066e98cebf9286
Nito <nikolas1997@hotmail.de>
2019-04-18 14:30:30 +0200

    Rope prototype using mouse
    Also some restructuring and test texture

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,24 +18,25 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        //get current orientation of camera to know where pressign W should make ball go
+        float vertInput = Input.GetAxis("Vertical"); //forward, backwards
+        float horInput = Input.GetAxis("Horizontal"); //left, right
+
+
+
+        //Get current orientation of camera to know where pressign W should make ball go
 
         Vector3 flattendForwardLookDir = Camera.main.transform.forward; //x und z werte, zeigen aber leicht in boden, wie kamera... daher normalisieren um nur auf boden entlang zu bewegen
         flattendForwardLookDir.y = 0;
         flattendForwardLookDir.Normalize();
-
         //print(flattendForwardLookDir);
 
-        float speed = Input.GetAxis("Vertical"); //forward, backwards
-        float speedSideways = Input.GetAxis("Horizontal"); //left, right
+        Vector3 rightMoveDir = Vector3.Cross(Vector3.up, flattendForwardLookDir);
 
-        rb.AddForce(flattendForwardLookDir * speed * moveSpeed, ForceMode.Impulse);
+        Vector3 moveDir = (flattendForwardLookDir * vertInput) + (rightMoveDir * horInput);
+        moveDir.Normalize();
 
+        rb.AddForce(moveDir * moveSpeed, ForceMode.Force); //Acceleration //Impulse //or change mass? //https://answers.unity.com/questions/789917/difference-and-uses-of-rigidbody-force-modes.html
 
-        var right = Vector3.Cross(Vector3.up, flattendForwardLookDir);
-
-        rb.AddForce(right * speedSideways * moveSpeed, ForceMode.Impulse);
-
-        //TODO: dont use addforce
+        //TODO: better controlls... dont use addforce? more gravity?
     }
 }
---
+++ Assets/Scripts/RopeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject anchorGO;
+    private Rigidbody anchorRB;
+    private SpringJoint joint;
+
+    void Start()
+    {
+        joint = GetComponent<SpringJoint>();
+        anchorRB = anchorGO.GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0)) //TODO: spawn anchor at mouse position (if raycast hits) and configure joint
+        {
+            if (joint == null)
+            {
+                anchorGO.SetActive(true);
+                gameObject.AddComponent<SpringJoint>();
+                joint = GetComponent<SpringJoint>();
+                joint.anchor = Vector3.zero;
+                joint.autoConfigureConnectedAnchor = false;
+                joint.connectedAnchor = Vector3.zero;
+                joint.spring = 10; //somehow calcualte from distance? Vector3.Distance(anchorGO.transform.position, transform.position)
+                joint.connectedBody = anchorRB;
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Destroy(joint);
+            anchorGO.SetActive(false);
+        }
+    }
+}
