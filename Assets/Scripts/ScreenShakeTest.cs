b53c40b8baaae3e9513ba3cf9e4507d8dbff9be8
Nito <nikolas1997@hotmail.de>
2019-05-12 16:40:27 +0200

    More dynamic shake

--- Assets/Scripts/ScreenShakeTest.cs
+++ Assets/Scripts/ScreenShakeTest.cs
@@ -68,6 +68,20 @@ public class ScreenShakeTest : MonoBehaviour
         dir *= Random.Range(-1f, 1f);
         dir.Normalize();
 
-        AddShake(dir, 1);
+        var am = player.GetComponent<PlayerController>().GetVelocity(); //And based on player speed
+        if (am > 20) am = 20;
+        am = am.Remap(0,20, 0.1f,1f);
+
+        AddShake(dir, am);
     }
+}
+
+public static class ExtensionMethods
+{
+
+    public static float Remap(this float value, float from1, float to1, float from2, float to2)
+    {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+    }
+
 }
