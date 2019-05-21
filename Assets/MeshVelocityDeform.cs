5fd80f068161843eec643e4ef3a27aa389befb5f
Nito <nikolas1997@hotmail.de>
2019-05-21 02:03:25 +0200

    Ball Deform Prototype

---
+++ Assets/MeshVelocityDeform.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshVelocityDeform : MonoBehaviour
+{
+    private Material mat;
+    private Rigidbody rb;
+    private Vector3 velocity = new Vector3(2,1,1);
+
+    void Start()
+    {
+        mat = GetComponent<MeshRenderer>().material;
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        velocity = rb.velocity;
+        mat.SetVector("Vector3_FF646A34", velocity);
+
+        print(velocity);
+    }
+}
