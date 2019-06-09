9127954556f733d7ce308cb189b28927add9bd43
Nito <nikolas1997@hotmail.de>
2019-06-10 00:24:17 +0200

    Fixed Build & Sorted

--- Assets/Scripts/MashGenerationEditor.cs
+++ Assets/Scripts/MashGenerationEditor.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
 using UnityEditor;
 
 [CustomEditor(typeof(MeshGenerator))]
@@ -17,3 +18,4 @@ public class MashGenerationEditor : Editor
             generatorScript.generateMesh();
     }
 }
+#endif
