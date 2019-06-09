a4b7415046a8f9f598c0cd24e40c694a18b0a970
loxer <loxer@live.de>
2019-06-09 18:52:30 +0200

    goal kinda finished

--- Assets/Scripts/VfxEffects/Goal.cs
+++ Assets/Scripts/VfxEffects/Goal.cs
@@ -1,18 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Experimental.VFX;
 
 public class Goal : MonoBehaviour
 {
+    [SerializeField] private VisualEffect goalEffect;
+    private GameObject player;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+        goalEffect.SetVector3("Goal", transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        goalEffect.SetVector3("Player", player.transform.position);
     }
 }
